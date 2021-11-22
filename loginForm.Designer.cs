aebd9bafcc58c229c8351756393a3a6a5f03ade7
Kin <88630282+KinIE213@users.noreply.github.com>
2021-11-22 14:06:27 +0800

    Add files via upload

---
+++ loginForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace AudioPlayer
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonRegistry = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(114, 144);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(223, 20);
+            this.textBoxUserName.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(114, 208);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(223, 20);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(114, 253);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(223, 23);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(157, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 42);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LOGIN";
+            // 
+            // buttonRegistry
+            // 
+            this.buttonRegistry.Location = new System.Drawing.Point(114, 299);
+            this.buttonRegistry.Name = "buttonRegistry";
+            this.buttonRegistry.Size = new System.Drawing.Size(223, 23);
+            this.buttonRegistry.TabIndex = 6;
+            this.buttonRegistry.Text = "Registry";
+            this.buttonRegistry.UseVisualStyleBackColor = true;
+            this.buttonRegistry.Click += new System.EventHandler(this.buttonRegistry_Click);
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 367);
+            this.Controls.Add(this.buttonRegistry);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUserName);
+            this.Name = "loginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonRegistry;
+    }
+}
---
+++ loginForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace AudioPlayer
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+        Hashtable account = new Hashtable();
+        private LinkedList<string> accountList = new LinkedList<string>();
+        public string CreateMD5Hash(string input)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+             if (account.ContainsKey(textBoxUserName.Text) && account.ContainsValue(CreateMD5Hash(textBoxPassword.Text)))
+             {             
+                musicPlayer form = new musicPlayer();        
+                form.Show();               
+            }               
+            else             
+            {                
+                MessageBox.Show("Invalid Input");               
+            }              
+        }
+
+        private void buttonRegistry_Click(object sender, EventArgs e)
+        {
+            String username = textBoxUserName.Text;
+            String password = textBoxPassword.Text;
+            using (var sw = new StreamWriter("account.csv"))
+            {
+                
+                account.Add(username, CreateMD5Hash(password));
+                 
+                accountList.AddLast(username + "," + CreateMD5Hash(password));
+                string security = username + "," + CreateMD5Hash(password);
+                sw.WriteLine(security);         
+            }
+        }
+    }
+}
